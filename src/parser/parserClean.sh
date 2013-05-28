5ee5b992a1eac9cba289751a8189350f2f326be7
gotclout <rlfosterjr@gmail.com>
2013-05-28 14:04:07 -0300

    Create parserClean.sh

---
+++ src/parser/parserClean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm RepositoryManager.g RepositoryManager.tokens RepositoryManagerLexer.h RepositoryManagerLexer.c RepositoryManagerParser.h RepositoryManagerParser.c;
+
+rm *.o;
+
+rm librepoparserlexer.so.1.0;
+
+exit 0;
